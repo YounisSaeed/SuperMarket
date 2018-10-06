cd958b79964e5009a671dec2e665371eb3e74993
Walid889 <walidsalahh66@gmail.com>
2018-10-06 03:47:50 +0200

    edit9

--- src/Manager_Account/Manager_AccountController.java
+++ src/Manager_Account/Manager_AccountController.java
@@ -5,8 +5,12 @@
  */
 package Manager_Account;
 
+import Manager.Main.HomeController;
+import java.io.IOException;
 import java.net.URL;
 import java.util.ResourceBundle;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
@@ -21,6 +25,7 @@ import javafx.scene.layout.AnchorPane;
  * @author NOUR
  */
 public class Manager_AccountController implements Initializable {
+    HomeController x = new HomeController(); // used for load windows
 
     @FXML
     private AnchorPane Account;
@@ -51,6 +56,25 @@ public class Manager_AccountController implements Initializable {
 
     @FXML
     private void Back(ActionEvent event) {
+        x.loadwindow(Account,"/Manager/Main/Home.fxml");
+    }
+
+    @FXML
+    private void Backup(ActionEvent event) {
+        try {
+            Runtime.getRuntime().exec("xcopy D:\\walid\\Last2\\Supermarket_Management_System\\database D:\\walid\\Last2\\DB /s/h/e/k/f/c/y/r");
+        } catch (IOException ex) {
+            Logger.getLogger(Manager_AccountController.class.getName()).log(Level.SEVERE, null, ex);
+        }
+    }
+
+    @FXML
+    private void Restore(ActionEvent event) {
+        try {
+            Runtime.getRuntime().exec("xcopy D:\\walid\\Last2\\DB D:\\walid\\Last2\\Supermarket_Management_System\\database /s/h/e/k/f/c/y/r");
+        } catch (IOException ex) {
+            Logger.getLogger(Manager_AccountController.class.getName()).log(Level.SEVERE, null, ex);
+        }
     }
     
 }
